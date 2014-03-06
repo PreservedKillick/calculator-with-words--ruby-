bbdd4888dc05883394ca34b748e56684ada0d748
Angela Yen <ayen10@gmail.com>
2014-03-06 14:39:45 -0800

    calculator with words. can perform one operation of add, subtract, multiple, and divide

--- lib/calculator_words.rb
+++ lib/calculator_words.rb
@@ -1,32 +1,19 @@
 def calculator_words(sentence)
   match_location =
-  math_opps = { "plus" => "+", "minus" => "-" }
+  math_opps = { "plus" => "+", "minus" => "-", "times" => "*", "divided" => "/" }
   sentence.delete!("?")
+  sentence.delete!("by")
   sentence_array = sentence.split.slice(2..-1)
   math_opps_keys = math_opps.keys
-    # ["plus", "minus"]
-    
-  match_location = sentence_array.each_index.select {|i| sentence_array[i] == math_opps.keys[0] }
-  sentence_array[match_location.join.to_i] = math_opps.values[0]
-  sum = eval sentence_array.join("")
-  sum
+      
+  intersections = math_opps_keys & sentence_array
+   intersections
 
+  match_location = sentence_array.each_index.select {|i| sentence_array[i] == intersections.join }
+  sentence_array[match_location.join.to_i] = math_opps.values_at(intersections.join)
+  result = eval sentence_array.join("")
+  result
 end
 
-print calculator_words("What is 18 minus 7?")
-
-#sentence_array.each_index.select {|i| sentence_array[i] == math_opps.keys[0]}
-
-  # sentence_array.map! do |i|
-  #   if sentence_array(i) == math_opps.keys[0]
-
-  #      sentence_array(i) = math_opps.values[0]
-  #   end
-  #   sentence_array[i].to_i
-  #   puts sentence_array
-  # end
-
-
-
-
+print calculator_words("What is 20 divided by 6?")
 
--- spec/calculator_words_spec.rb
+++ spec/calculator_words_spec.rb
@@ -11,4 +11,13 @@ describe ('calculator_words') do
   it("returns 11 for 'what is 18 minus 7?'") do
     calculator_words("What is 18 minus 7?").should(eq(11))
   end
+  it("returns 24 for 'What is 8 times 3?'") do
+    calculator_words("What is 8 times 3?").should(eq(24))
+  end
+  it("returns 4 for 'What is 8 divided by 2?'") do
+    calculator_words("What is 8 divided by 2?").should(eq(4))
+  end
+  it("returns 4 for 'What is 8 divided by 2?'") do
+    calculator_words("What is 8 divided by 2?").should(eq(4))
+  end
 end
